c5e27a42fd40b91d3c0ee9ce83fe5d2f9e74a597
roman <rcalderin@lya-electronic.com>
2024-12-28 17:17:18 -0500

    new configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,18 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import {Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
-import { NotificationsModule } from './notifications/notifications.module';
 import { ApiModule } from './api/api.module';
-import { DatabaseModule } from './database/database.module';
 import { PassportModule } from './passport/passport.module';
-import { Middleware } from './settings/middleware';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot({envFilePath: '.env', isGlobal:true}), 
-    AuthModule, UsersModule, DashboardModule, NotificationsModule, ApiModule, DatabaseModule, PassportModule],
+    AuthModule, UsersModule, DashboardModule, ApiModule, DatabaseModule, PassportModule],
   controllers: [AppController],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(Middleware).forRoutes('*');
-  }
+
 }
