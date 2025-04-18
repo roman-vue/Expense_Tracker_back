b019e5f4b6a530b12edc3640d16e6c8337872b0f
roman <desarrollador9@infinitumscitech.com>
2025-04-18 13:07:38 -0500

    :hammer: :sparkles: api operation

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,31 +1,47 @@
-import { Controller, Get, Post, Request, Body, Param } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Body, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { UsersDto } from '../users/dto/users.dto';
+
 @ApiTags('AUTH')
 @Controller('auth')
 export class AuthController {
-    constructor(private authService:AuthService){}
+    constructor(private authService: AuthService) {}
 
     @Post('login')
-    async login(@Body() AuthDto: AuthDto,@Request() req) {
-      return await this.authService.validateUser(AuthDto)
+    @ApiOperation({
+        summary: 'Login',
+        description: 'Authenticates a user with their credentials and returns an access token and a refresh token.'
+    })
+    async login(@Body() authDto: AuthDto) {
+        return await this.authService.validateUser(authDto);
     }
 
     @Post('refresh/:email/:refresh_token')
-    async refresh(@Param('email') email: string,@Param('refresh_token') refresh_token: string) {
-      return await this.authService.refreshToken(email, refresh_token)
+    @ApiOperation({
+        summary: 'Refresh token',
+        description: 'Takes a valid email and refresh token, and returns a new access token.'
+    })
+    async refresh(@Param('email') email: string, @Param('refresh_token') refresh_token: string) {
+        return await this.authService.refreshToken(email, refresh_token);
     }
 
     @Post('register')
+    @ApiOperation({
+        summary: 'Register new user',
+        description: 'Registers a new user in the system using the provided information.'
+    })
     async register(@Body() userDto: UsersDto) {
-      return await this.authService.register(userDto)
+        return await this.authService.register(userDto);
     }
 
     @Post('logout/:email')
-    async logout(@Param('email') email:string) {
-      return await this.authService.logout(email)
+    @ApiOperation({
+        summary: 'Logout',
+        description: 'Logs out the user and removes the refresh token associated with the provided email.'
+    })
+    async logout(@Param('email') email: string) {
+        return await this.authService.logout(email);
     }
-
 }
