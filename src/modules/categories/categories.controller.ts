7a1987d299e41c8ef8220054b97bdab92fb2e616
roman <desarrollador9@infinitumscitech.com>
2025-08-26 11:00:03 -0500

    :hammer: edit summery controllers

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request }
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { LocalGuard } from 'src/guards/local-guard/local-guard.guard';
 
 @UseGuards(LocalGuard)
@@ -12,30 +12,35 @@ export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
   @Post()
+  @ApiOperation({summary:'Create categorie'})
   create(@Body() createCategoryDto: CreateCategoryDto, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.categoriesService.create(createCategoryDto, userEmail);
   }
 
   @Get()
+  @ApiOperation({summary:'List categories'})
   findAll(@Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.categoriesService.findAll(userEmail);
   }
 
   @Get(':id')
+  @ApiOperation({summary:'Find categorie by id'})
   findOne(@Param('id') id: string, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.categoriesService.findOne(id, userEmail);
   }
 
   @Patch(':id')
+  @ApiOperation({summary:'Update categorie'})
   update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto , @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.categoriesService.update(id, updateCategoryDto, userEmail);
   }
 
   @Delete(':id')
+  @ApiOperation({summary:'Delete categorie'})
   remove(@Param('id') id: string, @Request() req) {
     const userEmail = req.headers['user-email'] as string;  
     return this.categoriesService.remove(id, userEmail);
--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Request, UseGuards, 
 import { WalletService } from './wallet.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { LocalGuard } from 'src/guards/local-guard/local-guard.guard';
 import { CreateTransactionsDto } from './dto/create-transactions.dto';
 
@@ -13,42 +13,49 @@ export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
   @Post()
+  @ApiOperation({summary: 'Created wallet'})
   create(@Body() createWalletDto: CreateWalletDto, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.create(createWalletDto, userEmail);
   }
 
   @Get()
+  @ApiOperation({summary: 'List wallet'})
   findAll(@Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.findAll(userEmail);
   }
 
   @Get(':id')
+   @ApiOperation({summary: 'Datils wallet with transactions'})
   findOne(@Param('id') id: string, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.findOne(id, userEmail);
   }
 
   @Patch(':id')
+  @ApiOperation({summary: 'updated wallet'})
   update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.update(id, updateWalletDto, userEmail);
   }
 
   @Delete(':id')
+  @ApiOperation({summary: 'deleted wallet'})
   remove(@Param('id') id: string, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.remove(id, userEmail);
   }
 
   @Post('/add-transactions/:walletId')
+  @ApiOperation({summary: 'Registrer transactions'})
   addTransactions(@Param('walletId') walletId: string, @Body() createTransactionsDto: CreateTransactionsDto, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.addTransactions(walletId, createTransactionsDto, userEmail);
   }
 
   @Put('/edit-transactions/:transactionId/:walletId')
+   @ApiOperation({summary: 'Edit transactions'})
   editTransactions(@Param('transactionId') transactionId: string, @Param('walletId') walletId: string, @Body() createTransactionsDto: CreateTransactionsDto, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.editTransactions(transactionId,walletId, userEmail, createTransactionsDto);
@@ -56,6 +63,7 @@ export class WalletController {
 
   
   @Delete('/delete-transactions/:transactionId/:walletId')
+     @ApiOperation({summary: 'Delete transactions'})
   deleteTransactions(@Param('transactionId') transactionId: string, @Param('walletId') walletId: string, @Request() req) {
     const userEmail = req.headers['user-email'] as string;
     return this.walletService.deleteTransactions(transactionId,walletId, userEmail);
